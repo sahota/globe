41bafa830e25a6e37363a86462c011e94ae6f3fd
gss121@hotmail.com <gss121@hotmail.com>
2013-03-06 02:06:43 +0000

    test

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Globe::Application.config.secret_token = '43bc73f8d19a29e16be16afddfbb04c237c0b2959bb6c1c938cee5148875e0d59b127a0f7dba91fc17ff8b7d60dc72bd37ff53ae1aa7514c6c5efdddccf56b20'
